78ad2306146b8181b6a209a72eed48037c112efa
Luis Cabrera <luiscaleb@outlook.com>
2023-11-09 19:48:35 +0100

    Prime number assignment

--- homework1.py
+++ homework1.py
@@ -1,34 +1,25 @@
-# user inputs N
-n = int(input("Enter the N number of prime numbers to print: "))
-
-# we create a list to store the prime numbers
-
-def is_prime(n):
-  """Returns True if n is a prime number, False otherwise."""
+def prime(n):
   if n <= 1:
-    return False
+    return False  # Returns True if n is a prime number, False otherwise
   for i in range(2, int(n**0.5) + 1):
     if n % i == 0:
       return False
   return True
 
-
-def print_n_primes(n):
-  """Prints the first n prime numbers."""
-  count = 0
-  num = 2
+def n_primes(n):
+  count = 0    #prints the firs N prime numbers
+  num = 2      # we start from the first prime number
   while count < n:
-    if is_prime(num):
+    if prime(num):
       print(num)
       count += 1
     num += 1
 
-
-# Get the value of N from the user
-N = int(input("How many prime numbers do you want to print? "))
+# user inputs N
+n = int(input("Enter the N number of prime numbers to print: "))
 
 # Print the first N prime numbers
-print_n_primes(N)
+n_primes(n)
 
 
 
